49c6c5c3371c7f27ceb708e3e64edab8de38b6e7
Stark-Xue <1539980567@qq.com>
2019-09-03 21:55:27 +0800

    blit ship on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
 	#初始化游戏并创建一个屏幕对象
@@ -12,6 +13,8 @@ def run_game():
 	
 	bg_color = (230, 230, 230)
 	
+	ship = Ship(screen)
+	
 	#开始游戏的主循环
 	while True:
 		
@@ -21,7 +24,8 @@ def run_game():
 				sys.exit()
 				
 		#每次循环时都重绘屏幕
-		screen.fill(ai_settings.bg_color)		
+		screen.fill(ai_settings.bg_color)	
+		ship.blitme()	
 				
 		#让最近绘制的屏幕可见
 		pygame.display.flip()
